9b3a2c84d257966ba13fc7255b8d755bb0cbef7f
k08a <etnallirb.a@gmail.com>
2016-11-26 04:23:36 +0000

    indexとshowアクション作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def new 
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(params_book)
+    @book.save
+  end
+  
+  private
+  def params_book
+    params.require(:book).permit(:name, :price, :publish, :publish_date
+    )
+  end
+    
 end
