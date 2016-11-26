393d276138ec79d520895b795820f92425eb8e51
k08a <etnallirb.a@gmail.com>
2016-11-26 09:23:18 +0000

    レビュー投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,8 @@ class BooksController < ApplicationController
   
   def show
   #   @book = Book.find(params[:id])
+  
+  # @reviews = Review.where(book_id: params[:id])
   end
   
   def new 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,7 +28,8 @@ class ReviewsController < ApplicationController
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        # format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to controller: :books, action: :show, id: @review.book.id, notice: "Review was successfully created." }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
